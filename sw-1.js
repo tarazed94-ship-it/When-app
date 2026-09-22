605d6811d3545e3c4b707f7d010a7086728c7f4e
tarazed94-ship-it <tarazed94@gmail.com>
2026-09-23 00:53:34 +0200

    Add files via upload

---
+++ sw-1.js
@@ -0,0 +1,22 @@
+const CACHE = 'when-app-v1';
+const SHELL = ['./', './index.html', './manifest.json', './icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+// Réseau en priorité (données trafic en direct), secours sur le cache hors-ligne
+self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
